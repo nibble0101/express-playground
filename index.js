8dfa57aec4db99f0951947ed654d6b5dcc8137aa
Joseph Mawa <mj.mawa@yahoo.com>
2022-12-06 22:06:30 +0300

    add form

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require("express");
+const path = require('path')
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.json({ port });
+  res.sendFile(path.join(__dirname, 'index.html'))
 });
+
+app.post('/submit', (req, res) => {
+  console.log(req.body.fname);
+  console.log(req.body.lname);
+  res.json(req.body)
+})
 
 app.listen( port, () => {
   console.log(`server listening on port ${port}`);
